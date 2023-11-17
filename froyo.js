d27d64965846de1ef30129e10651cff8159158a4
flowergirlthatcodes3 <149510282+flowergirlthatcodes3@users.noreply.github.com>
2023-11-17 13:50:55 -0500

    add new files add new files

---
+++ froyo.js
@@ -0,0 +1,4 @@
+const flavors =window.prompt("enter a list of flavors" , "vanilla,vanilla,vanilla,strawberry,coffee,coffee")
+console.log(flavors)
+const flavorsArr = flavors.split(',')
+console.log(flavorsArr)
